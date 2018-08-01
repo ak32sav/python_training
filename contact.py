3346c437b056bb1cf817b0d99258185f04217cbe
Andrew Sayenko <ak32sav@gmail.com>
2018-08-01 08:00:29 -0700

    Changed self.wd = WebDriver() to self.wd = WebDriver(capabilities={"marionette": False}) (tests weren't run properly)

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, fname, lname, nickname, home_phone, email):
+        self.fname = fname
+        self.lname = lname
+        self.nickname = nickname
+        self.home_phone = home_phone
+        self.email = email
---
+++ test_add_contact.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.firefox.webdriver import WebDriver
+import unittest
+from contact import Contact
+
+def is_alert_present(wd):
+    try:
+        wd.switch_to_alert().text
+        return True
+    except:
+        return False
+
+class test_add_contact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver(capabilities={"marionette": False})
+        self.wd.implicitly_wait(60)
+    
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_homepage(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd, Contact(fname="fname1", lname="lname1",nickname="nickname1",home_phone="0987654321",email="email1@gmail.com"))
+        self.open_homepage(wd)
+        self.logout(wd)
+
+    def create_contact(self, wd, contact):
+        # open contact creation page
+        wd.find_element_by_link_text("add new").click()
+        # fill the form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.fname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        # submit
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def open_homepage(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
