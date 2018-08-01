cc5ebae8bec39407c934dd3fa38092b60c612eb4
Andrew Sayenko <ak32sav@gmail.com>
2018-08-01 08:00:06 -0700

    Changed self.wd = WebDriver() to self.wd = WebDriver(capabilities={"marionette": False}) (tests weren't run properly)

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 from group import Group
 
@@ -14,7 +13,7 @@ def is_alert_present(wd):
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
+        self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
     
     def test_add_group(self):
