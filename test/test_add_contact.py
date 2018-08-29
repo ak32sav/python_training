b3bf603e70f1ebdd55213f02f8d8d2cdfdc3a2c9
Andrew Sayenko <ak32sav@gmail.com>
2018-08-29 04:18:54 -0700

    Task #8. Login/logout changes

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,5 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-        app.session.login(username="admin", password="secret")
         app.contact.create(Contact(fname="fname1", lname="lname1", nickname="nickname1", home_phone="0987654321", email="email1@gmail.com"))
-        app.session.logout()
 
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,10 @@
-
+from model.group import Group
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group()
-    app.session.logout()
+    app.group.edit_first_group(Group(name="Edited group name", header="Edited group header", footer="Edited group footer"))
+
+def test_edit_group_name(app):
+    app.group.edit_first_group(Group(name="New group name"))
+
+def test_edit_group_header(app):
+    app.group.edit_first_group(Group(header="New group header"))
