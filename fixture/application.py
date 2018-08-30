cd55103759423d7de704197e7a91703816583e54
Andrew Sayenko <ak32sav@gmail.com>
2018-08-30 01:44:29 -0700

    Task #10. Open a page only if current page is different

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,7 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False})
-        self.wd.implicitly_wait(5)
+        #self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -22,7 +22,8 @@ class Application:
 
     def open_homepage(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        if not wd.current_url == "http://localhost/addressbook/index.php":
+            wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
         self.wd.quit()
--- fixture/group.py
+++ fixture/group.py
@@ -5,10 +5,10 @@ class GroupHelper:
     def __init__(self,app):
         self.app=app
 
-
-    def return_to_groups_page(self):
+    def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/groups.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def create(self, group):
         wd = self.app.wd
@@ -18,7 +18,7 @@ class GroupHelper:
         self.fill_group_form(group)
         # submit group creation form
         wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -33,17 +33,13 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(field_value)
 
-    def open_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
-
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
         self.select_first_group()
         # delete group
         wd.find_element_by_name("delete").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
     def select_first_group(self):
         wd = self.app.wd
@@ -59,7 +55,7 @@ class GroupHelper:
         self.fill_group_form(new_group_data)
         # submit
         wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
+        self.open_groups_page()
 
     def count(self):
         wd = self.app.wd
