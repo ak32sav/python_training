ab14247c38dceff39bc02344f1c206b01c862f9a
Andrew Sayenko <ak32sav@gmail.com>
2018-09-03 07:22:47 -0700

    Task #10. Open a page only if current page is different

--- fixture/application.py
+++ fixture/application.py
@@ -22,7 +22,7 @@ class Application:
 
     def open_homepage(self):
         wd = self.wd
-        if not wd.current_url == "http://localhost/addressbook/index.php":
+        if not (wd.current_url == "http://localhost/addressbook/index.php" and len(wd.find_elements_by_name("new")) > 0):
             wd.get("http://localhost/addressbook/index.php")
 
     def destroy(self):
