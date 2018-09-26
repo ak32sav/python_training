8c00fcc1313cbb5bfb14ebf016ea7e27b8f0dfa9
Andrew Sayenko <ak32sav@gmail.com>
2018-09-26 11:27:49 -0700

    Lesson 7. test db

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -2,7 +2,7 @@ from fixture.application import Application
 from fixture.db import DbFixture
 from model.group import Group
 
-def test_grou_list(app, db):
+def test_group_list(app, db):
     ui_list = app.group.get_group_list()
     db_list = db.get_group_list()
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
